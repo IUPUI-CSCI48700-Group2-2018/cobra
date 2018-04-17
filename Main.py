7df4f4a0e342d8c697fa5471acf0e10496fe9dfb
ashcl <christopher.lee.ash@gmail.com>
2018-04-17 13:23:52 -0400

    Added a dataset cleaner for corrupted image files and updated NNet to use real dataset

--- Main.py
+++ Main.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+import fnmatch
+import os
+
+from PIL import Image
+
+
+def find_files(directory, pattern):
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                filename = os.path.join(root, basename)
+                yield filename
+
+
+for filename in find_files('./CarsID', '*'):
+    try:
+        img = Image.open(filename)
+        img.close()
+    except OSError:
+        print("Removing file: %f".format(filename))
+        os.remove(filename)
+
+print("Exit...")
