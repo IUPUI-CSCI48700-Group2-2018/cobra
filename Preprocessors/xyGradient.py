7472fd4d940a2be3e49c1e23294d53ed09a6862d
Amos Endersen <amenders@iupui.edu>
2018-04-21 22:37:54 -0400

    cleaned up imports

--- Preprocessors/xyGradient.py
+++ Preprocessors/xyGradient.py
@@ -1,10 +1,9 @@
 # source: https://stackoverflow.com/questions/49732726/how-to-compute-the-gradients-of-image-using-python/49735873#49735873
 
-import PIL
 import numpy as np
+import PIL
 import scipy
-from scipy import misc
-from scipy import ndimage
+from scipy import misc, ndimage
 
 def xyGradient(img):
     img = img.astype('int32')
